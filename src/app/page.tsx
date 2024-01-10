adeb6e61c73689840c1e84f574bc9e4abbef813e
emiliano lucero <emilianoglucero@gmail.com>
2024-01-10 00:37:46 -0300

    Refactor page component and add metadata generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import { SliceZone } from '@prismicio/react'
+import { Metadata } from 'next'
 
-import { Welcome } from '~/app/sections/welcome'
+import { createClient } from '~/prismicio'
+import { components } from '~/slices'
 
-const HomePage = () => {
+import s from './page.module.scss'
+
+export default async function Page() {
+  const client = createClient()
+  const home = await client.getSingle('home')
+  // console.log('home', home)
+
   return (
-    <>
-      <Welcome />
-    </>
+    <div className={s.home}>
+      <div className="home__wrapper">
+        <SliceZone slices={home.data.slices} components={components} />
+      </div>
+      <a className="home__link" href={'/collections'}>
+        {home.data.button}
+      </a>
+    </div>
   )
 }
 
-export default HomePage
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient()
+  const page = await client.getSingle('home')
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description
+  }
+}

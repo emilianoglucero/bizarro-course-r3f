6cfa3b768580aa80cf4a663442410df49cf58547
emiliano lucero <emilianoglucero@gmail.com>
2024-01-10 01:02:08 -0300

    Add content module and update content slice

--- src/slices/Content/index.tsx
+++ src/slices/Content/index.tsx
@@ -2,6 +2,7 @@ import { Content } from '@prismicio/client'
 import { PrismicNextImage } from '@prismicio/next'
 import { PrismicRichText, SliceComponentProps } from '@prismicio/react'
 
+import style from './content.module.scss'
 /**
  * Props for `Content`.
  */
@@ -12,13 +13,27 @@ export type ContentProps = SliceComponentProps<Content.ContentSlice>
  */
 const Content = ({ slice }: ContentProps): JSX.Element => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <p>{slice.primary.label}</p>
-      <PrismicRichText field={slice.primary.description} />
-      <PrismicNextImage field={slice.primary.image} />
+    <section className={style.content}>
+      <div
+        className={`${style.content__wrapper} ${
+          slice.primary.type === 'Right' ? style.content__right : ''
+        } `}
+      >
+        <div className={style.content__box}>
+          <p className={style.label}>{slice.primary.label}</p>
+          <div className={style.description}>
+            <PrismicRichText field={slice.primary.description} />
+          </div>
+        </div>
+        <div className={style.media}>
+          {/* <div className="media"> */}
+          {/* <div className={`${style.media} ${style.about}`}> */}
+          <PrismicNextImage
+            className={style.media__image}
+            field={slice.primary.image}
+          />
+        </div>
+      </div>
     </section>
   )
 }
